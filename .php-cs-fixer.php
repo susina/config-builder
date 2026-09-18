c1853c5b0562e019d2856343b5748f51cb3e8212
Cristiano Cinotti <cristianocinotti@gmail.com>
2026-09-18 17:37:22 +0200

    Adjust copyright information

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,18 +1,27 @@
-<?php declare(strict_types=1);
+<?php 
+
+declare(strict_types=1);
+
 /*
- * Copyright (c) Cristiano Cinotti 2024 - 2026.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  http://www.apache.org/licenses/LICENSE-2.0
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) Cristiano Cinotti
+ * 
+ * This file is part of susina/config-builder package, released under the APACHE-2 license.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 $finder = new PhpCsFixer\Finder()->in(__DIR__ . '/src')->in(__DIR__ . '/tests');
 
-return new PhpCsFixer\Config()->setRules(['@PER-CS3x0' => true])->setFinder($finder);
+return new PhpCsFixer\Config()
+    ->setRules([
+        '@PER-CS3x0' => true,
+        'header_comment' => [
+            'header' => "Copyright (c) Cristiano Cinotti
+
+This file is part of susina/config-builder package, released under the APACHE-2 license.
+For the full copyright and license information, please view the LICENSE file distributed 
+with this source code.",
+        ]
+    ])
+    ->setFinder($finder)
+;
--- tests/Functional/FunctionalTest.php
+++ tests/Functional/FunctionalTest.php
@@ -1,11 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
 /*
- * Apache-2 License.
+ * Copyright (c) Cristiano Cinotti
+ *
  * This file is part of susina/config-builder package, released under the APACHE-2 license.
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file distributed
+ * with this source code.
  */
 
 namespace Susina\ConfigBuilder\Tests\Functional;
